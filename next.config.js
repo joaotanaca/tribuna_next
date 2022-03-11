e63f7c428784b1078e14fc81209f3a7251773da0
Tanaca <jtanacar@gmail.com>
2022-03-11 18:58:33 -0300

    🔧 change config files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    webpack: (config) => {
+        config.experiments = { topLevelAwait: true, layers: true };
+        return config;
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
