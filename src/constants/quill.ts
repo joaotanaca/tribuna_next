73f01ab7c1592e1b283ac6f1553c312d64aa53f9
Tanaca <jtanacar@gmail.com>
2022-03-17 16:03:29 -0300

    🔧 add config and add path mapping

---
+++ src/constants/quill.ts
@@ -0,0 +1,14 @@
+export const toolbar = [
+    ["bold", "italic", "underline", "strike"],
+    [{ align: [] }],
+
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ indent: "-1" }, { indent: "+1" }],
+
+    [{ size: ["small", false, "large", "huge"] }],
+    [{ header: [3, 4, 5, 6, false] }],
+    ["link", "image", "video"],
+    [{ color: [] }, { background: [] }],
+
+    ["clean"],
+];
