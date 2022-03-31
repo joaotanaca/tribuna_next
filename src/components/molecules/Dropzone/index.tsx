4f370b519495d3a90e42f163dd03e01b36096333
Tanaca <jtanacar@gmail.com>
2022-03-31 14:02:37 -0300

    💄 add class to pseudo-classes in dropzone

--- src/components/molecules/Dropzone/index.tsx
+++ src/components/molecules/Dropzone/index.tsx
@@ -1,13 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
-import React, { useEffect, useMemo, useState } from "react";
+import { FileT } from "interfaces/dropzone";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import { getBase64 } from "../../../lib/base64";
 import { ContainerDrop } from "./styles";
 
 const Dropzone: React.FC<{
-    onChange: (img: string | string[]) => void;
+    onChange: (preview: FileT) => void;
 }> = ({ onChange }) => {
-    const [preview, setPreview] = useState("");
+    const [preview, setPreview] = useState({ name: "", img: "" });
     const {
         acceptedFiles,
         getRootProps,
@@ -21,40 +22,58 @@ const Dropzone: React.FC<{
         multiple: false,
     });
 
-    const files = acceptedFiles.map((file) => {
-        getBase64(file, setPreview);
-        return (
-            <div key={file.name} className="flex gap-4 items-center h-full">
-                <img
-                    src={preview}
-                    alt="img-preview"
-                    className="rounded-lg object-contain"
-                />
-                <b>{file.name}</b>
-            </div>
-        );
-    });
+    const dropActions = useMemo(() => {
+        if (isFocused) {
+            return "focus";
+        } else if (isDragAccept) {
+            return "accept";
+        } else if (isDragAccept) {
+            return "accept";
+        } else if (isDragReject) {
+            return "reject";
+        }
+        return "";
+    }, [isDragAccept, isDragReject, isFocused]);
+
+    const handlePreview = useCallback((key: string, value: string) => {
+        const objPreview: { [key: string]: string } = {};
+        objPreview[key] = value;
+        setPreview((prev) => ({ ...prev, ...objPreview }));
+    }, []);
+
     useEffect(() => {
         onChange(preview);
     }, [onChange, preview]);
 
+    useEffect(() => {
+        acceptedFiles.forEach((file) => {
+            handlePreview("name", file.name);
+            getBase64(file, (img: string) => handlePreview("img", img));
+        });
+    }, [acceptedFiles, handlePreview]);
+
     return (
-        <section className="container grid grid-cols-12 gap-4">
+        <section className="container grid grid-cols-10 gap-4">
             <ContainerDrop
                 {...getRootProps({
-                    className: `${
-                        isFocused
-                            ? "focus"
-                            : isDragAccept
-                            ? "accept"
-                            : isDragReject && "reject"
-                    } dropzone flex flex-col items-center p-5 rounded-md border border-dashed outline-none col-span-9`,
+                    className: `${dropActions} dropzone flex flex-col items-center justify-center p-5 rounded-md border border-dashed outline-none col-span-10`,
                 })}
             >
                 <input {...getInputProps()} />
                 <p>Selecione ou arraste uma imagem aqui</p>
             </ContainerDrop>
-            <aside>{files}</aside>
+            {acceptedFiles.length > 0 && (
+                <aside className="col-span-10">
+                    <div className="flex gap-4 items-center h-full preview-image">
+                        <img
+                            src={preview.img}
+                            width="100%"
+                            alt="img-preview"
+                            className="w-full rounded-lg object-cover"
+                        />
+                    </div>
+                </aside>
+            )}
         </section>
     );
 };
--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -7,6 +7,21 @@ import { PostKeys, PostModel } from "interfaces/models/Post";
 import Link from "next/link";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import Head from "next/head";
+import styled from "styled-components";
+import { lsset } from "lib/localstorage";
+
+const Container = styled.div.attrs({ className: "mx-auto" })`
+    max-width: 1080px;
+    .jodit-container {
+        text-align: justify;
+    }
+    .preview-image {
+        img {
+            height: 430px;
+        }
+    }
+`;
 
 const PostPage: React.FC = () => {
     const {
@@ -22,6 +37,15 @@ const PostPage: React.FC = () => {
     const [article, setArticle] = useState("");
     const [img, setImg] = useState("");
     const values = watch();
+
+    const date = new Date().toLocaleDateString("pt-BR", {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+    });
+
     const onSubmit = async (data: PostModel) => {
         try {
             const { data: response } = await axios.post<PostModel>(
@@ -30,6 +54,8 @@ const PostPage: React.FC = () => {
                     ...data,
                     image: img,
                     article,
+                    createdAt: date,
+                    updatedAt: date,
                 },
             );
             setLink(`/post/${response._id}/${response.title}`);
@@ -46,87 +72,93 @@ const PostPage: React.FC = () => {
 
     const onPreview = async () => {
         await axios.post<PostModel>("/api/preview");
-        localStorage.setItem(
-            "previewData",
-            JSON.stringify({
-                ...values,
-                image: img,
-                article,
-            }),
-        );
+
+        lsset("previewData", {
+            ...values,
+            image: img,
+            article,
+            createdAt: date,
+            updatedAt: date,
+        });
         window.open(`/post/preview/${values.title}`, "_blank")?.focus();
     };
 
     return (
         <>
-            {link && (
-                <div className="container mx-auto mt-16">
-                    <Link passHref href={link as any}>
-                        <a target="_blank">Post publicado</a>
-                    </Link>
-                </div>
-            )}
-            <form
-                className="flex flex-col gap-4 mx-auto container mt-16"
-                onSubmit={handleSubmit(onSubmit)}
-                onChange={() => {
-                    if (Object.keys(errors).length) clearErrors();
-                }}
-            >
-                <div className="grid grid-cols-2 gap-8">
-                    <div className="flex flex-col col-span-2">
-                        <Input
-                            placeholder="Titulo"
-                            type="text"
-                            error={errors.title?.message}
-                            name="title"
-                            register={register}
-                        />
-                    </div>
-                    <div className="flex flex-col col-span-1">
-                        <Input
-                            placeholder="Subtitulo"
-                            type="text"
-                            error={errors.subtitle?.message}
-                            name="subtitle"
-                            register={register}
-                        />
-                    </div>
-                    <div className="flex flex-col col-span-1">
-                        <Input
-                            placeholder="Autor"
-                            type="text"
-                            error={errors.authorship?.message}
-                            name="authorship"
-                            register={register}
-                        />
-                    </div>
-                    <div className="col-span-2">
-                        <Dropzone onChange={(img) => setImg(img as string)} />
-                    </div>
-                </div>
-                <div className="w-full">
-                    <Quill onChange={setArticle} />
-                </div>
+            <Head>
+                <title>Criação de Post</title>
+            </Head>
 
-                <div className="flex gap-4">
-                    <Button
-                        className="w-full"
-                        type="submit"
-                        disabled={isSubmitting}
-                    >
-                        {isSubmitting ? "Enviando" : "Enviar"}
-                    </Button>
-                    <Button
-                        className="w-full"
-                        onClick={onPreview}
-                        type="button"
-                        disabled={!isValid}
-                    >
-                        Preview
-                    </Button>
-                </div>
-            </form>
+            <Container>
+                {link && (
+                    <div className="container mx-auto mt-16">
+                        <Link passHref href={link as any}>
+                            <a target="_blank">Post publicado</a>
+                        </Link>
+                    </div>
+                )}
+                <form
+                    className="flex flex-col gap-4 mx-auto container mt-16"
+                    onSubmit={handleSubmit(onSubmit)}
+                    onChange={() => {
+                        if (Object.keys(errors).length) clearErrors();
+                    }}
+                >
+                    <div className="grid grid-cols-2 gap-8">
+                        <div className="flex flex-col col-span-2">
+                            <Input
+                                placeholder="Titulo"
+                                type="text"
+                                error={errors.title?.message}
+                                name="title"
+                                register={register}
+                            />
+                        </div>
+                        <div className="flex flex-col col-span-1">
+                            <Input
+                                placeholder="Subtitulo"
+                                type="text"
+                                error={errors.subtitle?.message}
+                                name="subtitle"
+                                register={register}
+                            />
+                        </div>
+                        <div className="flex flex-col col-span-1">
+                            <Input
+                                placeholder="Autor"
+                                type="text"
+                                error={errors.authorship?.message}
+                                name="authorship"
+                                register={register}
+                            />
+                        </div>
+                        <div className="col-span-2">
+                            <Dropzone onChange={(image) => setImg(image.img)} />
+                        </div>
+                    </div>
+                    <div className="w-full">
+                        <Quill onChange={setArticle} />
+                    </div>
+
+                    <div className="flex gap-4">
+                        <Button
+                            className="w-full"
+                            type="submit"
+                            disabled={isSubmitting}
+                        >
+                            {isSubmitting ? "Enviando" : "Enviar"}
+                        </Button>
+                        <Button
+                            className="w-full"
+                            onClick={onPreview}
+                            type="button"
+                            disabled={!isValid}
+                        >
+                            Preview
+                        </Button>
+                    </div>
+                </form>
+            </Container>
         </>
     );
 };
