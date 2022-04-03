503ee867892f7d46b6accc30887a1e2bd549fdc6
Tanaca <jtanacar@gmail.com>
2022-04-03 12:29:28 -0300

    🔧 change configuration's toastfy

--- src/constants/toast.ts
+++ src/constants/toast.ts
@@ -1,16 +1,13 @@
 import { AiOutlineCheck } from "react-icons/ai";
 import { UpdateOptions } from "react-toastify";
 
-export const defaultConfig = { autoClose: 3000 };
 
 export const acceptConfig: UpdateOptions = {
-    ...defaultConfig,
     icon: AiOutlineCheck,
     isLoading: false,
 };
 
 export const errorConfig: UpdateOptions = {
-    ...defaultConfig,
     type: "error",
     isLoading: false,
 };
